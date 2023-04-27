a7b695fa03a0830f50298a08d8b17081be4b759e
leenoru <tkdwns828282@gmail.com>
2023-04-27 16:27:22 +0900

    Initial commit

---
+++ spring-boot-project-re/src/main/java/com/playdata/springbootprojectre/SpringBootProjectReApplication.java
@@ -0,0 +1,13 @@
+package com.playdata.springbootprojectre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectReApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectReApplication.class, args);
+	}
+
+}
